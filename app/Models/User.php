21b0419342d8275ff574a3da83462bb9502c867f
relinquish3d <fluzyyyfly@gmail.com>
2026-09-18 07:53:58 +0700

    new fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    protected $primaryKey = 'id_user';
+    protected $primaryKey = 'id';
 
 
     /**
